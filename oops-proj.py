c149045f81b253588d905d8ed60986173480d15d
Kazi Md. Rakibul Hassan <kazrkb@gmail.com>
2026-03-19 12:29:35 +0600

    signed up and singed in implemented

---
+++ oops-proj.py
@@ -0,0 +1,52 @@
+class chatbook:
+    def __init__(self):
+        self.username = ""
+        self.password = ""
+        self.loggedin = False
+        self.menu()
+
+
+    def menu(self):
+        user_input = input("""Welcome to chatbook!! How would you like to proceed
+                           1. Press 1 to signup
+                           2. Press 2 to signin
+                           3. Press 3 to write a post
+                           4. Press 4 to message a friend
+                           5. Press any key to exit
+                           """)
+        if user_input == "1":
+            self.signup()
+        elif user_input == "2":
+            self.signin()
+        elif user_input == "3":
+            pass
+        elif user_input == "4":
+            pass
+        else:
+            exit()
+
+    def signup(self):
+        email = input("enter your email here ->")
+        pwd = input("enter your password here ->")
+        self.username = email
+        self.password = pwd
+        print("You have signed up successfully !!")
+        print("\n")
+        self.menu()
+
+    def signin(self):
+        if self.username=="" and self.password=="":
+            print("Please Signup First by pressing 1")
+            self.menu()
+        else: 
+            email = input("enter your email here ->")
+            pwd = input("enter your password here ->")
+            if self.username == email and self.password == pwd:
+                print("You have logged in successfully")
+                print("Want to go to main menu? press 0")
+                m = input("")
+                if m == "0":
+                    self.menu()
+
+
+obj = chatbook()
